3dae07ddc2d6d61e2dbaa8550667795660b2dbd2
Pradeep <deeppradeep1998@gmail.com>
2024-10-31 16:05:23 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,13 @@
+my_dict={'a':1,'b':2,'c':3}
+
+#result = my_dict.update({'d':4})
+
+#print(result)
+
+#None will be the output
+
+my_dict.update({'d':4})
+
+print(my_dict)
+
+#{'a': 1, 'b': 2, 'c': 3, 'd': 4}
